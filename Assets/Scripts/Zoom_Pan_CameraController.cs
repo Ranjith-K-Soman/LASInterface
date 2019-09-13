13504293b2afaac88a2df336c365d5912e934a34
Ranjith K Soman <ranjithks17@imperial.ac.uk>
2019-09-13 18:37:34 +0100

    Initial Commit

---
+++ Assets/Scripts/Zoom_Pan_CameraController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom_Pan_CameraController : MonoBehaviour
+{
+    public float turnSpeed = 4.0f;         // Speed of camera turning when mouse moves in along an axis
+    public float panSpeed = 2.0f;			// Speed of the camera when being panned
+    private float moveSpeed = 1f;
+    private float scrollSpeed = 20f;
+
+    private Vector3 mouseOrigin;    // Position of cursor when mouse dragging starts
+    private bool isPanning;     // Is the camera being panned?
+    private bool isRotating;	// Is the camera being rotated?
+    private bool isZooming;
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            Vector3 pos = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            Vector3 move = new Vector3(pos.x * panSpeed/5, 0, pos.z * panSpeed/5);
+            transform.Translate(move, Space.Self);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+
+            Vector3 pos = new Vector3(0, 0, Input.GetAxis("Mouse ScrollWheel"));
+
+
+            Vector3 move = pos.z * scrollSpeed * transform.forward;
+            transform.Translate(move, Space.World);
+
+        }
+
+        
+
+        // Get the right mouse button
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            // Get mouse origin
+            mouseOrigin = Input.mousePosition;
+            isRotating = true;
+            
+        }
+
+        // Get the middle mouse button
+        if (Input.GetMouseButtonDown(2))
+        {
+            // Get mouse origin
+            mouseOrigin = Input.mousePosition;
+            isPanning = true;
+        }
+
+        // Disable movements on button release
+        if (!Input.GetMouseButton(1)) isRotating = false;
+        if (!Input.GetMouseButton(2)) isPanning = false;
+        //if(Input.GetAxis("Mouse ScrollWheel") == 0) isZooming = false;
+
+        // Rotate camera along X and Y axis
+        if (isRotating)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+            transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+            transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+        }
+
+        // Move the camera on it's XY plane
+        if (isPanning)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+            Vector3 move = new Vector3(pos.x * panSpeed, pos.y * panSpeed, 0);
+            transform.Translate(move, Space.Self);
+        }
+
+    
+
+    }
+}
+
---
+++ Assets/Scripts/dragobject.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragobject : MonoBehaviour
+{
+
+    private Vector3 mOffset;
+    private float mZCoord;
+    public Transform SpawnPointContainer;      // Assign parent gameobject of spawn points.
+    private Transform[] spawnPoints;
+    //private void Start()
+    //{
+        // Initialize spawnPoints array.
+      //  spawnPoints = new Transform[.childCount];
+        // Populate array with child transforms.
+        //for (int i = 0; i < spawnPointContainer.childCount; i++)
+        //{
+          //  spawnPoints[i] = spawnPointContainer.GetChild(i);
+        //}
+    //}
+    void OnMouseDown()
+
+    {
+
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+
+
+        // Store offset = gameobject world pos - mouse world pos
+
+        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+
+    }
+    private Vector3 GetMouseAsWorldPoint()
+
+    {
+
+        // Pixel coordinates of mouse (x,y)
+
+        Vector3 mousePoint = Input.mousePosition;
+
+
+
+        // z coordinate of game object on screen
+
+        mousePoint.z = mZCoord;
+
+
+
+        // Convert it to world points
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+
+    }
+
+    void OnMouseDrag()
+
+    {
+
+        transform.position = GetMouseAsWorldPoint() + mOffset;
+
+    }
+
+    // Start is called before the first frame update
+
+}
